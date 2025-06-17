4f7a9d78f15fb1e98b21bb148228d75e7d91b265
Sibo Wang <sibo.wang@epfl.ch>
2025-06-17 15:58:28 +0200

    change error logging to thrown exception in case behavior video can't be opened

--- src/spotlight_tools/common/video.py
+++ src/spotlight_tools/common/video.py
@@ -6,8 +6,7 @@ from pathlib import Path
 def get_video_info(video_path: Path):
     video = cv2.VideoCapture(str(video_path))
     if not video.isOpened():
-        logging.error(f"Error: Could not open video {video_path}.")
-        return None
+        raise RuntimeError(f"Error: Could not open video {video_path}.")
 
     width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
