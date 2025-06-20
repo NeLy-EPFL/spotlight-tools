179ae89fbd72782613b65b30fdd8e8a7fe151f81
Sibo Wang <sibo.wang@epfl.ch>
2025-06-20 11:40:28 +0200

    Implement behavior dataloader with cached cv2.VideoCapture object

--- src/spotlight_tools/common/dataloader.py
+++ src/spotlight_tools/common/dataloader.py
@@ -12,9 +12,15 @@ def load_muscle_image(recording_dir: Path, muscle_frame_id: int) -> np.ndarray:
     return cv2.imread(str(muscle_image_path), cv2.IMREAD_UNCHANGED)
 
 
-def load_behavior_frame(recording_dir: Path, behavior_frame_id: int) -> np.ndarray:
-    behavior_video_path = recording_dir / "processed/behavior_video.mkv"
-    behavior_video_capture = cv2.VideoCapture(str(behavior_video_path))
+def load_behavior_frame(
+    recording_dir: Path,
+    behavior_frame_id: int,
+    behavior_video_capture: cv2.VideoCapture | None = None,
+) -> np.ndarray:
+    cache_behavior_video_capture = behavior_video_capture is not None
+    if not cache_behavior_video_capture:
+        behavior_video_path = recording_dir / "processed/behavior_video.mkv"
+        behavior_video_capture = get_behavior_video_capture(behavior_video_path)
     num_frames = int(behavior_video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
     if behavior_frame_id >= num_frames:
         return None  # Return None if the frame ID is out of bounds
@@ -22,5 +28,13 @@ def load_behavior_frame(recording_dir: Path, behavior_frame_id: int) -> np.ndarr
     ret, frame = behavior_video_capture.read()
     if not ret:
         raise ValueError(f"Could not read frame {behavior_frame_id} from video.")
-    behavior_video_capture.release()
+    if not cache_behavior_video_capture:
+        behavior_video_capture.release()
     return frame[:, :, 0]
+
+
+def get_behavior_video_capture(behavior_video_path: Path) -> cv2.VideoCapture:
+    behavior_video_capture = cv2.VideoCapture(str(behavior_video_path))
+    if not behavior_video_capture.isOpened():
+        raise RuntimeError(f"Error: Could not open video {behavior_video_path}.")
+    return behavior_video_capture
