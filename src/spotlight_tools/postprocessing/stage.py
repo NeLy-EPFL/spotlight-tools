c127bd2babb0abfb414ce0f8b98f9885cebb0970
Sibo Wang <sibo.wang@epfl.ch>
2025-11-24 17:13:32 +0100

    for unknown reasons, behavior frame acquisition times in 20250613-fly1b-014 is not monotonically increasing (there's a backward jump about 2/3 into the recording). Maybe something went wrong during the recording. For now add a check to detect these situations.

--- src/spotlight_tools/postprocessing/stage.py
+++ src/spotlight_tools/postprocessing/stage.py
@@ -1,4 +1,5 @@
 import logging
+import numpy as np
 import pandas as pd
 from scipy.interpolate import CubicSpline
 from pathlib import Path
@@ -10,15 +11,15 @@ def interp_stage_pos_at_behavior_frames(
     frames_dir: Path, stage_positions_path: Path, output_path: Path
 ) -> pd.DataFrame:
     """Interpolate stage positions at behavior frame timestamps.
-    
+
     This function combines behavior frame timing metadata with stage position
     data to produce interpolated XY coordinates for each behavior frame.
-    
+
     Args:
         frames_dir (Path): Directory containing behavior frame CSV metadata files.
         stage_positions_path (Path): Path to the stage positions CSV file.
         output_path (Path): Path where the merged metadata will be saved.
-        
+
     Returns:
         pd.DataFrame: DataFrame with behavior frame metadata and interpolated
             stage positions (columns: behavior_frame_id, received_time_us,
@@ -31,7 +32,13 @@ def interp_stage_pos_at_behavior_frames(
     files_by_frame = find_files_per_frame_by_suffix(frames_dir, ".csv", stride=3)
     dataframes = [pd.read_csv(file) for file in files_by_frame.values()]
     concatenated_df = pd.concat(dataframes, ignore_index=True)
-    concatenated_df.sort_values(by=["received_time_us"], inplace=True)
+    acq_times_diff = np.diff(concatenated_df["acquired_time_us"].values)
+    if not np.all(acq_times_diff > 0):
+        logger.error(
+            "Acquisition times of behavior frames are not strictly increasing! "
+            "Something went wrong during the recording."
+        )
+        raise ValueError("Non-increasing acquisition times detected.")
     concatenated_df.reset_index(drop=True, inplace=True)
 
     # Rename "frame_id" column to "behavior_frame_id"
