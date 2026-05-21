97b6812129f3007d49923140d34f9e4738248a48
Sibo Wang <sibo.wang@epfl.ch>
2026-05-21 18:16:42 +0200

    fix config file path

--- src/spotlight_tools/common/config.py
+++ src/spotlight_tools/common/config.py
@@ -6,8 +6,8 @@ from pathlib import Path
 
 
 def load_spotlight_tools_config() -> dict:
-    spotlight_package_dir = files("spotlight_tools", "assets") / "model_config.yaml"
-    config_path = spotlight_package_dir.parent.parent / "config/config.yaml"
+    assets_dir = files("spotlight_tools", "assets")
+    config_path = assets_dir / "model_config.yaml"
     if not config_path.exists():
         raise FileNotFoundError(
             f"Configuration file {config_path} does not exist. Make sure the "
